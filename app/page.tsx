9f4dd1105cde68fd832a97a1dda0474fb57e4444
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2024-03-06 02:26:49 +0900

    '/blog' 글 목록에 사용될 컴포넌트 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
 import Image from "next/image";
+import { metadata } from "./layout";
+import RecentPosts from "@/components/RecentPosts";
 import Container from "@/components/Container";
 
 export default function Home() {
   return (
-  
-  <Container>
-    <span className={`font-bold`}>HOME</span>
-  </Container>
-  )
+    <Container>
+      <div className={`my-5 w-full`}>
+        <div className={`relative`}>
+          <Image
+            src={`/home.jpg`}
+            alt="대표 이미지"
+            width={`100`}
+            height={45}
+            layout={`responsive`}
+            objectFit="cover"
+            className={`rounded-3xl`}
+          />
+          <span
+            className={`absolute top-12 font-extrabold italic text-white text-5xl md:text-9xl text flex justify-center w-full drop-shadow-lg`}
+          >
+            {metadata.title}
+          </span>
+        </div>
+        <RecentPosts />
+      </div>
+    </Container>
+  );
 }
