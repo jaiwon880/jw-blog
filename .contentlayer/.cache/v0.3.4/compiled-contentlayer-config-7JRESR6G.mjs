9651250275e84c23ee6dfaeb3cd1f363aff7a7b8
jaiwon880 <rabbitmarry@gmail.com>
2024-05-17 16:38:25 +0900

    [240517] update

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-7JRESR6G.mjs
@@ -0,0 +1,35 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  contentType: "mdx",
+  filePathPattern: `**/*.mdx`,
+  fields: {
+    title: { type: "string", required: true },
+    date: { type: "string", required: true },
+    description: { type: "string", required: true },
+    category: {
+      type: "string",
+      required: true
+    },
+    thumbnail: {
+      type: "string",
+      required: false
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (post) => post._raw.sourceFileName.replace(".mdx", "")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "posts",
+  documentTypes: [Post]
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-7JRESR6G.mjs.map
