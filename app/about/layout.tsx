d1d4bda3a04b456883db83bb359d32d1439e0b17
rabbitmarry@gmail.com <rabbitmarry@gmail.com>
2024-07-19 23:09:45 +0900

    [240719] contentlayer 변경 준비, About 소개글 수정

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -2,7 +2,7 @@ import PageLayout from "@/components/PageLayout";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <PageLayout title="Joy Han" description="반갑습니다. 히히">
+    <PageLayout title="Joy Han" description="Backend Developer">
       {children}
     </PageLayout>
   );
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,8 +13,12 @@ export default function AboutPage() {
             className="rounded"
             quality={100}
           />
-          <span className="break-keep leading-8">
-            <b>어떤 분야의 전문가가 되기 위해서는 최소 1만 시간 정도의 훈련이 필요하다.</b>
+          <span className="break-keep leading-9">
+            <b>서비스 시장의 공백을 인식하고 이를 해결하기 위해 다양한 서비스를 기획하고 개발하는 데 집중하고 있습니다. </b>
+            <br></br>
+            <b>또한, 사용자의 다양한 행동 패턴을 분석하고 가정하며, 서비스의 빈틈을 찾아 이를 해결하는 것에 흥미를 느낍니다. </b>
+            <p>무엇보다도, 제가 개발한 서비스가 출시되어 고객의 문제를 해결하는 순간에 큰 보람을 느낍니다. </p>
+            <p>이러한 과정을 통해 고객의 니즈를 충족시키고 가치를 전달하며 더 좋은 서비스를 개발하며 성장하고 있습니다. </p>
           </span>
         </div>
       );
